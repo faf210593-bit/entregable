0132b852afe6dd4cd7a6283d3b65418732a7d7fb
faf210593-bit <faf.210593@gmail.com>
2025-11-06 17:59:16 -0500

    commit 3

---
+++ src/main/java/LegacyBillingSystem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class LegacyBillingSystem {
+  public void generateBill(String data) {
+        // Simula generar una factura antigua (imprime en consola)
+        System.out.println("Factura generada con LegacyBillingSystem: " + data);
+    }   
+}
