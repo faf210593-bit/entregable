638825d467d8a613d2e5a0d0effbba8698d19117
faf210593-bit <faf.210593@gmail.com>
2025-11-06 18:07:49 -0500

    commit 4

---
+++ src/main/java/com/mycompany/validacionservice/FacturaAdapter.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.validacionservice;
+
+interface FacturaService {
+    void generarFactura(String nombre, String producto, int cantidad, double total);
+}
+public class FacturaAdapter {
+private LegacyBillingSystem legacySystem;
+    public FacturaAdapter() {
+        this.legacySystem = new LegacyBillingSystem();
+    }
+    @Override
+    public void generarFactura(String nombre, String producto, int cantidad, double total) {
+        // Adapta: convierte los parámetros en un String que LegacyBillingSystem entienda
+        String data = nombre + " - " + producto + " x" + cantidad + " Total: " + total;
+        legacySystem.generateBill(data);  // Llama al método antiguo
+    }    
+}
